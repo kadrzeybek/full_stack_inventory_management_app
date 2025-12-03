5a9f0ffc6b8be924e338c63a080fb56ae6e31bc0
Kadir Zeybekoğlu <kadrzeybek@gmail.com>
2025-12-03 22:40:48 +0300

    project client setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Dashboard from '@/app/dashboard/page';
+
+export default function Home() {
+  return (
+    <div className='min-h-screen flex items-center justify-center text-white font-sans text-3xl'>
+      <Dashboard />
+    </div>
+    
+  );
+}
