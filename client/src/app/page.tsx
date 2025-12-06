53b584d63dd23f1415631dda03a717f0a909b46c
Kadir Zeybekoğlu <kadrzeybek@gmail.com>
2025-12-06 23:45:49 +0300

    navbar template completed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,7 @@ import Dashboard from '@/app/dashboard/page';
 
 export default function Home() {
   return (
-    <div className='min-h-screen flex items-center justify-center text-white font-sans text-3xl'>
+    <div>
       <Dashboard />
     </div>
     
